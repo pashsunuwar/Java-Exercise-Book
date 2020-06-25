d3c8cbfce61dc7c34ff095c16e6d586fc82eee19
pashsunuwar <psunuwar@qa.com>
2020-06-25 19:01:50 +0100

    added exe 1 and 2 solutions

---
+++ src/com/qa/calculator/Calculator.java
@@ -0,0 +1,44 @@
+package com.qa.calculator;
+import java.util.*;
+
+public class Calculator {
+	
+
+	public static void main(String[] args) {
+	
+	int firstnumber;
+	int secondnumber;
+	String operator;
+	
+	Scanner myCalc = new Scanner(System.in);
+	
+	System.out.println("Enter your first number: ");
+	firstnumber = myCalc.nextInt();
+	
+	System.out.println("Enter your second number: ");
+	secondnumber = myCalc.nextInt();
+	
+	System.out.println("Choose Arithmetic Operator from '+ - * /': ");
+	operator = myCalc.next();
+	
+	switch (operator) {
+	case "+" :
+		System.out.println("Adding the two operands equal to " + (firstnumber + secondnumber));
+		break;	
+	case "-" :
+		System.out.println("Subtracting the two operands equal to " + (firstnumber - secondnumber));
+		break;
+	case "*" :
+		System.out.println("Multiplying both operands equal to " + (firstnumber * secondnumber));
+		break;
+	case "/" :
+		System.out.println("Divinding both operands equal to " + (firstnumber / secondnumber));
+		break;
+	default:
+		System.out.println("Please use a valid operator. ");
+		break;
+	}
+	}
+	
+
+}
---
+++ src/com/qa/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.qa.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String [] args) {
+		System.out.println("HelloWorld");
+
+		String message = "HelloWorld";
+		System.out.println(message);
+
+		printText("HelloWorld");
+
+	}
+
+	public static void printText(String text) {
+		System.out.println(text);
+	}
+}
